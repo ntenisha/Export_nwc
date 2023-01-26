8e6479bf9c7f5b847531261eb136b037a9862c0c
Матвей <91886143+ntenisha@users.noreply.github.com>
2023-01-26 17:37:56 +0300

    Add files via upload

---
+++ Vigruzka.bat
@@ -0,0 +1,22 @@
+chcp 65001
+SetLocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION 
+Set VentFolder=X:\00_BIM\14_Академика Волгина\04_VENT
+Set EomFolder=X:\00_BIM\14_Академика Волгина\06_EOM
+Set SSFolder=X:\00_BIM\14_Академика Волгина\07_SS
+Set PtFolder=X:\00_BIM\14_Академика Волгина\05_PT
+
+Set ext1=*.nwc
+Set ext2=*.nwd
+del /f /q "%VentFolder%\%ext1%"		"%VentFolder%\%ext2%"
+del /f /q "%EomFolder%\%ext1%"		"%EomFolder%\%ext2%"
+del /f /q "%SSFolder%\%ext1%"		"%SSFolder%\%ext2%"
+del /f /q "%PtFolder%\%ext1%"		"%PtFolder%\%ext2%"
+
+
+start "" "C:\Program Files\Autodesk\Navisworks Manage 2019\FiletoolsTaskRunner.exe" /i "C:\Users\713\Desktop\Navis_volgina\Vent.txt" 	/od "%VentFolder%"	/version 2019
+start "" "C:\Program Files\Autodesk\Navisworks Manage 2019\FiletoolsTaskRunner.exe" /i "C:\Users\713\Desktop\Navis_volgina\Eom.txt" 	/od "%EomFolder%"	/version 2019
+start "" "C:\Program Files\Autodesk\Navisworks Manage 2019\FiletoolsTaskRunner.exe" /i "C:\Users\713\Desktop\Navis_volgina\SS.txt" 		/od "%SSFolder%"	/version 2019
+start "" "C:\Program Files\Autodesk\Navisworks Manage 2019\FiletoolsTaskRunner.exe" /i "C:\Users\713\Desktop\Navis_volgina\Pt.txt" 		/od "%PtFolder%"	/version 2019
+
+endlocal
+pause
---
+++ Vigruzka2.bat
@@ -0,0 +1,46 @@
+chcp 65001
+SetLocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+rem MainF это место проекта , кавычки обязательно
+Set MainF="X:\00_BIM\14_Академика Волгина\04_VENT\1111"
+rem Folders папки в которых нужно сделать навис модели
+set Folders[0]=04_VENT
+set Folders[1]=06_EOM
+set Folders[2]=07_SS
+set Folders[3]=05_PT
+rem LsI это номер последнего индекса строкой выше
+set LsI=3
+
+
+Set MainF2=%MainF:~1,-1%
+Set ext=*.rvt
+Set ext1=*.nwc
+Set ext2=*.nwd
+
+rem удалются nwc и nwd модели
+for /L %%i in (0,1,LsI) do (
+    del /f /q "%MainF2%\!Folders[%%i]!\%ext1%"	"%MainF2%\!Folders[%%i]!\%ext2%"
+)
+
+rem созадется пустой текстовый файл в каждой папке
+for /L %%i in (0,1,LsI) do (
+    cd.>%MainF%\!Folders[%%i]!\!Folders[%%i]!.txt
+)
+
+rem в текстовый файл прописываются все rvt модели в папке (без подпапок)
+for /L %%i in (0,1,LsI) do (
+    For /F "Delims=" %%A in ('dir /b %MainF%\!Folders[%%i]!\%ext%') do echo %MainF2%\!Folders[%%i]!\%%A>>%MainF%\!Folders[%%i]!\!Folders[%%i]!.txt
+)
+
+rem запускается навис
+for /L %%i in (0,1,LsI) do (
+    start "" "C:\Program Files\Autodesk\Navisworks Manage 2019\FiletoolsTaskRunner.exe" /i "%MainF2%\!Folders[%%i]!\!Folders[%%i]!.txt" 	/od 	"%MainF2%\!Folders[%%i]!"	/version 2019
+)
+
+rem чистим за собой. удалются txt файлы
+for /L %%i in (0,1,LsI) do (
+    del /f /q "%MainF2%\!Folders[%%i]!\!Folders[%%i]!.txt"
+)
+
+endlocal
+rem pause можно удалить
+pause
